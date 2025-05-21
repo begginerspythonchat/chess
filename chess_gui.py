0474d652188aea20ac30dcd3e95acb1192231228
Begginers Python Chat <begginerspythonchat@gmail.com>
2025-05-22 02:42:15 +0400

    GUI Chessboard - создание шахматной доски
    Шахматная доска (Chessboard) включает в себя шахматные поля (ChessField), поля доступны через self.fields .

--- chess_gui.py
+++ chess_gui.py
@@ -2,6 +2,39 @@ import tkinter
 from tkinter import ttk
 
 
+class ChessBoard(ttk.Frame):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields = []
+        font = ("fixed", 30, "normal")
+        white_black_switcher = 0
+        for ir, row in enumerate(" 87654321 "):
+            for ic, column in enumerate(" abcdefgh "):
+                if row == " ":
+                    ttk.Label(self, text=column, font=font).grid(
+                        row=ir, column=ic, sticky="nesw")
+                    continue
+                elif column == " ":
+                    ttk.Label(self, text=row, font=font).grid(
+                        row=ir, column=ic, sticky="nesw")
+                    continue
+                elif row == column == " ":
+                    ttk.Label(self, text="\u2003").grid(
+                        row=ir, column=ic, sticky="nesw")
+                    continue
+                color = ["white", "black"][white_black_switcher]
+                if color == "black":
+                    background = "grey"
+                else:
+                    background = "white"
+                field = ChessField(root, font=font, background=background)
+                field.grid(in_=self, row=ir, column=ic, sticky="nesw")
+                self.fields.append(field)
+                white_black_switcher ^= 1
+            white_black_switcher ^= 1
+
+
 class ChessField(ttk.Label):
 
     UNICODE_FIGURES = {
@@ -34,20 +67,7 @@ class ChessField(ttk.Label):
         self._figure = figure
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     root = tkinter.Tk()
-    row = column = switcher = 0
-    for figure in ChessField.UNICODE_FIGURES:
-        if column == 6:
-            row += 1
-            switcher ^= 1
-            column = 0
-        if not switcher:
-            background = "grey"
-        else:
-            background = "white"
-        cf = ChessField(root, font="Arial 50", background=background)
-        cf.figure = figure
-        cf.grid(in_=root, row=row, column=column)
-        column += 1
-        switcher ^= 1
+    chess_board = ChessBoard(root)
+    chess_board.pack(in_=root, fill="both", expand=True)
